4d5d00869178d71c708ad5da4ba9e98febe129c4
Jeet Parikh <jeetparikh06@gmail.com>
2025-05-16 18:14:24 -0700

    onrender prep

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ import uuid
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
